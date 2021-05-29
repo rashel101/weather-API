6326c68c37243131bf6ff4aca52e36eecd9dc842
rashel101 <rashelsariol@gmail.com>
2021-05-29 00:08:21 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let weather = {
+    apiKey: "15ba0e5af7b988b3ae62f24c1dd66829",
+    getWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerHTML = "Weather in " + name;
+      document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerHTML = description;
+      document.querySelector(".temp").innerHTML = temp + "°F";
+      document.querySelector(".humidity").innerHTML ="humidity: " + humidity + "%";
+      document.querySelector(".wind").innerHTML ="wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      //images from unsplah that change with each 
+      document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+      this.getWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.getWeather("Wilmington");
